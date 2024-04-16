8b389f86dc50c56a4a76f92886386ba4fc8332a9
Seong <dhkdclxk1996@gmail.com>
2024-04-17 00:51:26 +0900

    deploy_test:4

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     apps: [
       {
-        name: "playground_local", // 생성할 프로세스의 이름
+        name: "playground_prod", // 생성할 프로세스의 이름
         script: "dist/src/main.js", // 프로세스 생성 시 pm2 start를 할 경로(보통 서버 파일)
         env: {
-            NODE_ENV: 'local'
+            NODE_ENV: 'prod'
             //나의 local mysql 연결
         },
         env_production: {
