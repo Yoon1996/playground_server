c972f6b6e7397b63965b7ffd2395864fe6b6a532
Seong <dhkdclxk1996@gmail.com>
2024-04-21 17:22:31 +0900

    deploy_test_41

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'origin', 'X-Requested-With', 'Accept', 'Authorization'],
     exposedHeaders: ['Authorization'],
     credentials: true,
   })
