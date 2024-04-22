fbf44b08a31245e2440059329d11cc9d7323b184
Seong <dhkdclxk1996@gmail.com>
2024-04-22 16:31:56 +0900

    deploy_test_46

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  // const corsOptions = {
-  //   origin: ['https://d19ms2sq0w2bcn.cloudfront.net', 'http://d19ms2sq0w2bcn.cloudfront.net'],
-  //   methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'], // 필요한 HTTP 메서드 지정
-  //   allowedHeaders: ['Content-Type', 'Authorization'], // 필요한 헤더 지정
-  //   preflightContinue: false,
-  //   optionsSuccessStatus: 200,
-  // };
-  // app.enableCors({
-  //   origin: "*",
-  //   methods: ["GET", "POST", "PUT", "DELETE"],
-  //   allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'Authorization'],
-  //   exposedHeaders: ['Authorization'],
-  //   credentials: true,
-  // })
-  // app.enableCors(corsOptions)
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -26,7 +12,7 @@ async function bootstrap() {
       transform: true
     })
   )
-  await app.listen(3000);
+  await app.listen(433);
   //dddfdf
 }
 bootstrap();
